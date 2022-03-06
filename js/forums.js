e8bcd2c3661b8408d1c0b30be27763dc8971588e
Craig Mudzingwa <tanxyrogue@gmail.com>
2022-03-06 22:02:16 +0100

    clarified comment on forums.js

--- js/forums.js
+++ js/forums.js
@@ -4,7 +4,7 @@ const digitalGamingBtn = document.querySelector('.forums-topic-options.digital')
 
 // creates an event that runs whenever the 'physical gaming' button button is clicked
 physicalGamingBtn.addEventListener('click', () => { 
-    // changes the page once the button is clicked to the forums-topic-view page, the code after the ? mark is additional info that tells the forums-view-page the button that was pressed to get to it and change information page according
+    // changes the page once the button is clicked to the forums-topic-view page, the code after the ? mark is a url parameter(additional info} that tells the forums-view-page the button that was pressed to get to it and change information page according
     window.location = './forums-topic-view.html?prev=physical'
 });
 
