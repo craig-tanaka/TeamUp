16e1da8b267d0a6d3a26e5b35641016d07a2d194
Craig Mudzingwa <tanxyrogue@gmail.com>
2022-03-08 10:44:11 +0100

    added firebase Login functionality

--- js/login.js
+++ js/login.js
@@ -1,12 +1,47 @@
 // grabs all the login button on the page and places it in a constant
 const loginBtn = document.querySelector('#login-submit-input');
-
-//since the app doesnt have an online database , the folowing creates a reference to browsers sessionStorage for storage that shared bewteen pages
-myStorage = window.sessionStorage;
+// gets error label and stores reference to it in a constant
+const errorLabel = document.querySelector('.login-error-label');
 
 // adds an event that is run when login btn is pressed
 loginBtn.addEventListener('click', (e) => {
-    // event first emulates a login by creating a variable in localstorage
-    myStorage.setItem('loggedIn', 'Yes');
-    window.location = './index.html';
+
+    if (validateForm()) {
+        document.querySelector('.details-row.submit').style.display = 'none';
+        errorLabel.innerHTML = 'Loging in......';
+
+        firebase.auth().signInWithEmailAndPassword(document.querySelector('#login-email-input').value,
+                                                document.querySelector('#login-password-input').value)
+        .then((userCredential) => {
+            window.location = './index.html';
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            document.querySelector('.details-row.submit').style.display = 'flex';
+            alert(errorMessage);
+            errorLabel.innerHTML = errorMessage;
+        });
+    } else {
+        alert(errorLabel.innerHTML);
+    }
 })
+
+// Checks if user has filled in inputs
+function validateForm() { 
+    let hasNoErrors = true;
+
+    
+    if (document.querySelector('#login-password-input').value === '')
+        hasNoErrors = false;
+    if (document.querySelector('#login-email-input').value === '') { 
+        hasNoErrors = false;
+        if (!hasNoErrors) errorLabel.innerHTML = 'Please Fill in Missing Labels!!!!!';
+    }
+    else if (!document.querySelector('#login-email-input').value.includes('@') || !document.querySelector('#login-email-input').value.includes('.')) { 
+        hasNoErrors = false;
+        errorLabel.innerHTML = 'Email is not in Correct Format';
+    }
+    
+    return hasNoErrors;
+}
