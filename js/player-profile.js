81e45a1e61d7b5baa4cb6dbdff55c5e01f81c416
Craig Mudzingwa <tanxyrogue@gmail.com>
2022-03-10 00:28:26 +0100

    added functionality to send message button

--- js/player-profile.js
+++ js/player-profile.js
@@ -33,3 +33,46 @@ storageReference.child(`/playerCardPics/${requestedPlayerUid}/profile-picture.jp
             var img = document.querySelector('.player-profile-picture');
             img.setAttribute('src', url);
         })
+
+// creates a window where the user can a leave a message for the profile they are viewing
+document.querySelector('#leave-a-message-btn').addEventListener('click', () => { 
+    let messageWindow = document.createElement('div');
+    messageWindow.className = 'message-window-container';
+
+    messageWindow.innerHTML = `
+        <div class='message-window-overlay'></div>
+        <div class='message-window'>
+            <label for="message-text" class="message-text-label">Leave a Message for ${playerName.innerHTML}</label>
+            <textarea name="message-text" id="message-text-input" cols="30" rows="2"></textarea>
+            <input type="submit" value="Submit" class="message-text-submit">
+        </div>`;
+    document.querySelector('body').appendChild(messageWindow);
+
+    document.querySelector('.message-text-submit').addEventListener('click', () => { 
+        document.querySelector('.message-text-label').innerHTML = 'Sending Message...';
+        document.querySelector('.message-text-submit').style.display = 'none';
+        // create player card document in database
+        db.collection(`playerMessages`).doc(requestedPlayerUid).collection('messages').doc(firebase.auth().currentUser.uid).set({
+            from: userDoc.userName,
+            senderID: firebase.auth().currentUser.uid,
+            message: document.querySelector('#message-text-input').value
+        })
+        .then((docRef) => {
+            // console.log("Document written");
+            document.querySelector('.message-text-label').innerHTML = 'Sent';
+            setTimeout(() => document.querySelector('body').removeChild(messageWindow), 750);
+
+        })
+        .catch((error) => {
+            console.error("Error adding image: ", error);
+        });
+    })
+
+    
+    document.querySelector('.message-window-overlay').addEventListener('click', () => { 
+        messageWindow.removeChild(document.querySelector('.message-window'));
+        messageWindow.removeChild(document.querySelector('.message-window-overlay'));
+    })
+})
+
+// disable leave a message button if user is viewing their own profile
